f49f21a4b2d79766e97a25d9493ae9f7530fd407
Abhishek <abhi.l127375@gmail.com>
2026-05-08 00:09:02 +0530

    Daily Track of Backend

---
+++ BACKEND/Day-22-OrganizationPage-FolderStructure/backend/api/routes/auth.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { signup, signin, getuser, signout } from '../controllers/auth.controller.js';
+import { protect } from '../middleware/auth.middleware.js';
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/signin", signin);
+
+router.get("/get-users", protect, getuser)
+router.post("/signout", signout)
+
+export default router;
